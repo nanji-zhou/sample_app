0f6ff317776f618057414d981a755cce54632902
nanji-zhou <zmlzml136@hotmail.com>
2016-06-14 17:25:28 +1000

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # by Ming: helper methods need to be used on some Controllers
+  include SessionsHelper
 end
